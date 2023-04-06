5c9b2e038d629f04b2c9922d0b6302ef4bffbb34
YIOXIO <gekkoivan@gmail.com>
2023-04-06 14:26:44 +0300

    fix: refactor

--- scripts/my-project.js
+++ scripts/my-project.js
@@ -1,8 +1,15 @@
 const openFormButton = document.querySelector('.my-projects__button');
 const projectForm = document.querySelector('.project-form')
+const closeFormButton = document.querySelector('.project-form__reset-button')
 
 function openForm() {
     projectForm.classList.add('project-form_opened')
 }
 
+function closeForm() {
+    projectForm.classList.remove('project-form_opened')
+}
+
 openFormButton.addEventListener('click', openForm)
+closeFormButton.addEventListener('click', closeForm)
+
