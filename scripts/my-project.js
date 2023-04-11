77c97e2db228e628c0675edfd8ebb44024871021
YIOXIO <gekkoivan@gmail.com>
2023-04-11 15:19:07 +0300

    updated

--- scripts/my-project.js
+++ scripts/my-project.js
@@ -1,6 +1,14 @@
-const openFormButton = document.querySelector('.my-projects__button');
-const projectForm = document.querySelector('.project-form')
+const openFormButton = document.querySelector('.my-projects__button')
 const closeFormButton = document.querySelector('.project-form__reset-button')
+const cardTemplate = document.querySelector('.card-template').content.querySelector('.card');
+const elementsCard = document.querySelector('.elements__card');
+const projectForm = document.querySelector('.project-form');
+const inputName = projectForm.querySelector('#name-input');
+const inputDesc = projectForm.querySelector('#description-input');
+const inputImage = projectForm.querySelector('#image-input');
+const submitFormButton = projectForm.querySelector('#submit-button');
+
+
 
 function openForm() {
     projectForm.classList.add('project-form_opened')
@@ -10,6 +18,44 @@ function closeForm() {
     projectForm.classList.remove('project-form_opened')
 }
 
+
+function createCard(evt) {
+    const card = cardTemplate.cloneNode(true);
+    card.querySelector('.card__image').src = evt.image;
+    card.querySelector('.card__name').textContent = evt.name;
+    card.querySelector('.card__description').textContent = evt.desc
+    elementsCard.append(card)
+    return card;
+
+}
+
+inputImage.addEventListener('change', function () {
+    const reader = new FileReader()
+
+    reader.addEventListener('load', () => {
+        console.log(reader.result)
+        localStorage.setItem('image', reader.result);
+    })
+
+    reader.readAsDataURL(this.files[0])
+})
+
+function handleSubmitFormAddNewCard(evt) {
+    evt.preventDefault()
+    const newCard = {
+        name: inputName.value,
+        desc: inputDesc.value,
+        image: inputImage.value
+    }
+    createCard(newCard)
+    projectForm.reset()
+    closeForm()
+}
+
+
+
+
+
+projectForm.addEventListener('submit', handleSubmitFormAddNewCard);
 openFormButton.addEventListener('click', openForm)
 closeFormButton.addEventListener('click', closeForm)
-
